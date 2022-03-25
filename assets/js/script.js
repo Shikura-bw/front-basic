fb70b5f3a161ca64ebda938c7cdb1727672de37a
Shikura-bw <n_shikura@beeworks.co.jp>
2022-03-26 02:26:27 +0900

    [modify] 「スウォッチ管理」追加

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,19 @@ window.addEventListener("load",function(){
         body.classList.toggle('nav-bar');
     });
 });
+
+function equalHeight(elements) {
+    const target = Array.from(document.querySelectorAll(elements));
+    const heightList = [];
+    target.forEach(element => {
+      const height = element.clientHeight;
+      heightList.push(height);
+    });
+    const maxHeight = Math.max.apply(null,heightList);
+    target.forEach(element => {
+      element.style.height = maxHeight + 'px';
+    });
+  }
+  if(!navigator.userAgent.match(/(iPhone|Android)/)){
+    equalHeight('.js-height');
+  }
