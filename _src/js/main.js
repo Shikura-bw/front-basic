16ce3f108fdf03ebf99ed9c574a131de97b612a3
Shikura <n_shikura@beeworks.co.jp>
2022-06-03 16:00:41 +0900

    [modify] 展開ページ

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,14 +1,1 @@
 import FadeIn from './parts/common.js';
-
-const app = {
-  // 初期化
-  init() {
-    this.fadein = new FadeIn();
-  },
-};
-
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', app.init);
-} else {
-  app.init();
-}
