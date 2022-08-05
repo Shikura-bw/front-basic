8abad6f460dc3a43d8d0a1431b3f7f8de4330b88
Shikura <n_shikura@beeworks.co.jp>
2022-08-05 19:02:38 +0900

    [modfiy] ejs+json展開

--- README.md
+++ README.md
@@ -1,49 +1,10 @@
-## 概要
-- テンプレートエンジン: EJS
-- スタイル: SCSS(Webpack)
-- JSコンパイル: Webpack
-- ローカルサーバ: webpack-dev-server(Buildしないとdestされません！)
-
 ## 環境構築
-- `yarn install` : インストール
+- `yarn install` : モジュールインストール
 
 ## コマンド
 - `yarn watch` : ローカルサーバ起動、watch開始
 - `yarn build`: EJS,SCSS,JSのビルド、画像圧縮
 
-## ディレクトリ構造
-```
-├── README.md
-├── _src                      <- config.jsから変更可能
-│   ├── assets                <- 基本assets系は全部ここに
-│   │   ├── images            <- 圧縮前画像入れる
-│   │   ├── js
-│   │   │   └── index.js      <- entrypointとなるJS
-│   │   └── scss
-│   │       └── index.scss    <- entrypointとなるSCSS
-│   ├── template              <- HTMLは全部ここ
-│   │   └── index.html
-│   └── data.json             <- テンプレート内で使う変数置き場
-├── config.js                 <- srcとdestのパスを格納してます
-├── dist                      <- config.jsから変更可能
-│   ├── assets
-│   │   ├── images            <- 圧縮後の画像が入る
-│   │   ├── css
-│   │   │   └── index.css     <- コンパイル後のCSS
-│   │   └── js
-│   │       └── app.js        <- コンパイル後のJS
-│   └── index.html            <- コンパイル後のHTML
-├── babel.config.js
-├── config.js
-├── gulpfile.js
-├── imagemin.js
-├── package.json
-├── postcss.config.js
-├── webpack.config.js
-└── yarn.lock
-```
-
-## コーディングルール
-## 納品ファイル精製方法
-## Gitについて
-## その他
+## コーディング
+template.ejsをテンプレとし、pages.jsonにてページ情報を設定し、
+最終的にビルドする事によって個別にhtmlを書き出す。
--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,39 @@
-const config = require('./config')
 const gulp = require('gulp')
-const watch = require('gulp-watch')
 const notify = require('gulp-notify')
 const plumber = require('gulp-plumber')
 const ejs = require('gulp-ejs')
 const rename = require('gulp-rename');
+const fs = require('fs')
 
 // ----------------------------------------- EJS
-gulp.task('ejs', () => {
-  return gulp.src([
-    config.path.src + 'template/[^_]*.ejs',
-    config.path.src + 'template/[^_]**/[^_]*.ejs',
-    config.path.src + 'template/[^_]**/[^_]**/[^_]*.ejs',
-    config.path.src + 'template/[^_]**/[^_]**/[^_]**/[^_]*.ejs'
-  ])
-    .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
-    .pipe(ejs())
-    .pipe(rename({ extname: '.html' }))
-    .pipe(gulp.dest(config.path.dest))
+
+const src = './src'
+
+gulp.task('ejs', (done) => {
+    const jsonData = '_src/pages.json';
+    const json = JSON.parse(fs.readFileSync(jsonData));
+    const pages = json.pages;
+    const template = '_src/template/template.ejs';
+
+    for (let i = 0; i < pages.length; i++) {
+      gulp.src(template)
+        .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
+        .pipe(ejs({
+          jsonData: pages[i],
+          dir: pages[i].dir
+        }))
+        .pipe(rename((path) => {
+          path.dirname += pages[i].dir;
+          path.basename = pages[i].name;
+          path.extname = '.html';
+        }))
+        .pipe(gulp.dest('./dist'));
+    }
+    done();
 })
 
 gulp.task('watch', () => {
-  watch(config.path.src + 'template/**/*.ejs', gulp.series('ejs'))
+  gulp.watch(src + '/template/**/*.ejs', gulp.series('ejs'))
 })
 
 gulp.task('default', gulp.series('ejs', 'watch'))
